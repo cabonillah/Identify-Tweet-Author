26193974589af179b02a17f6dd3522b22c3f7f9c
Camilo Andrés Bonilla Hernández <ca_m_ilo@hotmail.com>
2022-12-06 16:58:44 -0500

    Final training attempt tuning

--- scripts/run_models.R
+++ scripts/run_models.R
@@ -13,13 +13,13 @@ runner({
     # autoplot(result, metric = "accuracy")
     # ggsave("../stores/elastic_clas.png")
 
-    # source("../scripts/rf_clas.R")
-    # autoplot(result, metric = "accuracy")
-    # ggsave("../stores/rf_clas.png")
-
-    source("../scripts/xgb_clas.R")
+    source("../scripts/rf_clas.R")
     autoplot(result, metric = "accuracy")
-    ggsave("../stores/xgb_clas.png")
+    ggsave("../stores/rf_clas.png")
+
+    # source("../scripts/xgb_clas.R")
+    # autoplot(result, metric = "accuracy")
+    # ggsave("../stores/xgb_clas.png")
 
     # source("../scripts/mlp_clas.R")
     # autoplot(result, metric = "accuracy")
